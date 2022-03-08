1c16e7d8f94b45f4b21c463a6bfc33959b19a4b1
Jane-Wandia <janekigenyi2018@gmail.com>
2022-03-09 01:27:00 +0300

    added getelementbyid

--- index.js
+++ index.js
@@ -4,3 +4,9 @@ let daysWeek = ['Sunday','Monday','Tuesday','Wednesday','Thursday','Friday','Sat
 let namesMale = ['Kwasi','Kwadwo','Kwabena','Kwaku','Yao','Kofi','Kwame']
 
 let namesFemale = ['Akosua','Adwoa','Abena','Akua','Yaa','Afua','Amma']
+
+
+// communicating with the form
+
+let form = document.getElementById('btn');
+
