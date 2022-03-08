ef06aa6423c4ed84435eaa67b7410a374d440f6c
Jane-Wandia <janekigenyi2018@gmail.com>
2022-03-09 01:22:53 +0300

    added 2 functions

--- index.js
+++ index.js
@@ -3,3 +3,4 @@ let daysWeek = ['Sunday','Monday','Tuesday','Wednesday','Thursday','Friday','Sat
 
 let namesMale = ['Kwasi','Kwadwo','Kwabena','Kwaku','Yao','Kofi','Kwame']
 
+let namesFemale = ['Akosua','Adwoa','Abena','Akua','Yaa','Afua','Amma']
